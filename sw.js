ce012748f40ed381184446a6b7a76f197b5daf4e
emviqdesigns-droid <emviq.designs@gmail.com>
2026-03-24 22:32:59 +0530

    Initial commit of Adoqa Analytics

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'adoqa-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './assets/Logopit_1773295062339.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(() => {
+        if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+        }
+    })
+  );
+});
